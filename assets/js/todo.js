879b796bf2091a17e6e96f91e88fa3e6a39548fd
james-adcott-edu <jadcott@gmail.com>
2024-11-18 21:07:47 +0000

    refactor: functions in alphabetical order

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -23,7 +23,7 @@ let todoList = [];
  */
 const addTodo = function(todo) {
     todoList.push(todo);
-    saveToLocalStorage();
+    localStorageSave();
 }
 
 /**
@@ -36,43 +36,6 @@ const addTodoHandler = function() {
     addTodo(createTodoItem(title));
     displayAllTodo();
     document.getElementById('todo-title').value = '';
-}
-
-/**
- * @typeDef {function} removeTodo
- * @param {number} id - The id of the todo to remove
- * @description Remove a todo from the list
- * @returns {void}
- * @example removeTodo(1)
- */
-const removeTodo = function(id) {
-    todoList = todoList.filter(todo => todo.id !== id);
-    saveToLocalStorage();
-}
-
-/**
- * @typeDef {function} removeTodoHandler
- * @param {number} id - The id of the todo to remove
- * @description Remove a todo from the list
- * @returns {void}
- */
-const removeTodoHandler = function(id) {
-    removeTodo(id);
-    displayAllTodo();
-}
-
-/**
- * @typeDef {function} generateId
- * @description Generate an id for a new todo item
- * @returns {number}
- */
-const generateId = function() {
-    let maxId = window.localStorage.getItem('maxId');
-    if (!maxId) maxId = 0;
-    if (maxId < todoList.length) throw new Error('maxId is greater than todoList length');
-    maxId++;
-    window.localStorage.setItem('maxId', maxId);
-    return maxId;
 }
 
 /**
@@ -92,30 +55,93 @@ const createTodoItem = function(title, detail='') {
 }
 
 /**
- * @typeDef {function} setTodoCompleted
- * @param {number} id - The id of the todo item to set as completed
- * @description Set a todo item as completed
+ * @typeDef {function} createInputTodoHTML
+ * @description Create the input todo HTML
+ * @returns {string}
+ */
+const createInputTodoHTML = function() {
+    return `
+        <div>
+        <form onsubmit="addTodoHandler()">
+        <input type="text" id="todo-title">
+        <button id="add-todo" type="submit">Add</button></div>
+        </form>
+        <p>${todoList.length} items in Todo List</p>
+        <hr>
+    `;
+}
+
+/**
+ * @typeDef {function} displayAllTodo
+ * @description Display all todo in the list
  * @returns {void}
  */
-const setTodoCompleted = function(id) {
-    todoList = todoList.map(todo => {
-        if (todo.id === id) {
-            todo.completed = !todo.completed;
-        }
-        return todo;
+const displayAllTodo = function() {
+    appDiv.innerHTML = createInputTodoHTML();
+    todoList.slice().reverse().forEach(todo => { // reverse the list to display the latest todo first
+        const todoDiv = document.createElement('div');
+        todoDiv.innerHTML = `
+                <p>
+                    <input type="checkbox" ${todo.completed ? 'checked' : ''} onchange="setTodoCompletedHandler(${todo.id})">
+                    <span data-completed="${todo.completed}">${todo.title}</span>
+                </p>
+                <button onclick="displayTodoDetail(${todo.id})">Detail</button>
+                <button onclick="removeTodoHandler(${todo.id})">Remove</button>
+            `;
+        appDiv.appendChild(todoDiv);
     });
 }
 
 /**
- * @typeDef {function} setTodoCompletedHandler
- * @param {number} id - The id of the todo item to set as completed
- * @description Set a todo item as completed
+ * @typeDef {function} displayTodoDetail
+ * @param {number} id - The id of the todo item to display
+ * @description Display the detail of a todo item
  * @returns {void}
  */
-const setTodoCompletedHandler = function(id) {
-    setTodoCompleted(id);
-    saveToLocalStorage();
-    displayAllTodo();
+const displayTodoDetail = function(id) {
+    const todo = todoList.find(todo => todo.id === id);
+    appDiv.innerHTML = `
+        <button onclick="displayAllTodo()">Back</button>
+        <div><input type="text" id="display-todo-title" value="${todo.title}"></div>
+        <div><textarea id="todo-detail">${todo.detail}</textarea></div>
+        <div><input id="todo-completed" type="checkbox" ${todo.completed ? 'checked' : ''}> Completed</div>
+        <button onclick="modifyTodoHandler(${todo.id})">Save</button>
+    `;
+}
+
+/**
+ * @typeDef {function} generateId
+ * @description Generate an id for a new todo item
+ * @returns {number}
+ */
+const generateId = function() {
+    let maxId = window.localStorage.getItem('maxId');
+    if (!maxId) maxId = 0;
+    if (maxId < todoList.length) throw new Error('maxId is greater than todoList length');
+    maxId++;
+    window.localStorage.setItem('maxId', maxId);
+    return maxId;
+}
+
+/**
+ * @typeDef {function} localStorageSave
+ * @description Save the todo list to local storage
+ * @returns {void}
+ */
+const localStorageSave = function() {
+    localStorage.setItem('todoList', JSON.stringify(todoList));
+}
+
+/**
+ * @typeDef {function} localStorageGet
+ * @description Get the todo list from local storage
+ * @returns {void}
+ */
+const localStorageGet = function() {
+    const todoListString = localStorage.getItem('todoList');
+    if (todoListString) {
+        todoList = JSON.parse(todoListString);
+    }
 }
 
 /**
@@ -148,81 +174,58 @@ const modifyTodoHandler = function(id) {
     let detail = document.getElementById('todo-detail').value;
     let completed = document.getElementById('todo-completed').checked;
     modifyTodo(id, title, detail, completed);
-    saveToLocalStorage();
+    localStorageSave();
     displayAllTodo();
 }
 
 /**
- * @typeDef {function} saveToLocalStorage
- * @description Save the todo list to local storage
+ * @typeDef {function} removeTodo
+ * @param {number} id - The id of the todo to remove
+ * @description Remove a todo from the list
  * @returns {void}
+ * @example removeTodo(1)
  */
-const saveToLocalStorage = function() {
-    localStorage.setItem('todoList', JSON.stringify(todoList));
+const removeTodo = function(id) {
+    todoList = todoList.filter(todo => todo.id !== id);
+    localStorageSave();
 }
 
 /**
- * @typeDef {function} getFromLocalStorage
- * @description Get the todo list from local storage
+ * @typeDef {function} removeTodoHandler
+ * @param {number} id - The id of the todo to remove
+ * @description Remove a todo from the list
  * @returns {void}
  */
-const getFromLocalStorage = function() {
-    const todoListString = localStorage.getItem('todoList');
-    if (todoListString) {
-        todoList = JSON.parse(todoListString);
-    }
+const removeTodoHandler = function(id) {
+    removeTodo(id);
+    displayAllTodo();
 }
 
 /**
- * @typeDef {function} createInputTodoHTML
- * @description Create the input todo HTML
- * @returns {string}
- */
-const createInputTodoHTML = function() {
-    return `
-        <div><input type="text" id="todo-title">
-        <button id="add-todo" onclick="addTodoHandler()">Add</button></div>
-        <p>${todoList.length} items in Todo List</p>
-        <hr>
-    `;
-}
-
-/**
- * @typeDef {function} displayAllTodo
- * @description Display all todo in the list
+ * @typeDef {function} setTodoCompleted
+ * @param {number} id - The id of the todo item to set as completed
+ * @description Set a todo item as completed
  * @returns {void}
  */
-const displayAllTodo = function() {
-    appDiv.innerHTML = createInputTodoHTML();
-    todoList.slice().reverse().forEach(todo => {
-        const todoDiv = document.createElement('div');
-        todoDiv.innerHTML = `
-                <p>
-                    <input type="checkbox" ${todo.completed ? 'checked' : ''} onchange="setTodoCompletedHandler(${todo.id})">
-                    <span data-completed="${todo.completed}">${todo.title}</span>
-                </p>
-                <button onclick="displayTodoDetail(${todo.id})">Detail</button>
-                <button onclick="removeTodoHandler(${todo.id})">Remove</button>
-            `;
-        appDiv.appendChild(todoDiv);
+const setTodoCompleted = function(id) {
+    todoList = todoList.map(todo => {
+        if (todo.id === id) {
+            todo.completed = !todo.completed;
+        }
+        return todo;
     });
 }
 
 /**
- * @typeDef {function} displayTodoDetail
- * @param {number} id - The id of the todo item to display
- * @description Display the detail of a todo item
+ * @typeDef {function} setTodoCompletedHandler
+ * @param {number} id - The id of the todo item to set as completed
+ * @description Set a todo item as completed
  * @returns {void}
  */
-const displayTodoDetail = function(id) {
-    const todo = todoList.find(todo => todo.id === id);
-    appDiv.innerHTML = `
-        <div><input type="text" id="display-todo-title" value="${todo.title}"></div>
-        <div><textarea id="todo-detail">${todo.detail}</textarea></div>
-        <div><input id="todo-completed" type="checkbox" ${todo.completed ? 'checked' : ''}> Completed</div>
-        <button onclick="modifyTodoHandler(${todo.id})">Save</button>
-        <button onclick="displayAllTodo()">Back</button>
-    `;
+const setTodoCompletedHandler = function(id) {
+    setTodoCompleted(id);
+    localStorageSave();
+    displayAllTodo();
 }
 
 /**
@@ -231,7 +234,7 @@ const displayTodoDetail = function(id) {
  * @returns {void}
  */
 const init = function() {
-    getFromLocalStorage();
+    localStorageGet();
     displayAllTodo();
 }
 
